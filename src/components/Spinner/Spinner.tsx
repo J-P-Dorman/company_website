a475f4af0510729087dfd69e48f380bd3b818105
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-06 16:37:38 +0000

    Add loading spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import "./Spinner.scss";
+
+const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <span className="spinner"></span>
+    </div>
+  );
+};
+
+export default Spinner;

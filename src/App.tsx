f2cd3d904106140d9510c609f0a93099b5a409df
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-06 16:45:33 +0000

    Format file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import "./App.scss";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.scss";
 import Home from "./pages/Home/Home";
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import { store } from "./redux/store";
 
 const App = () => {
-
-
   return (
     <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 };

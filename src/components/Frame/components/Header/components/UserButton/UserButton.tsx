e4387befda1b629a33afdadd89c4f691050baed0
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-06 16:44:55 +0000

    Use new auth token

--- src/components/Frame/components/Header/components/UserButton/UserButton.tsx
+++ src/components/Frame/components/Header/components/UserButton/UserButton.tsx
@@ -1,16 +1,58 @@
-import { useState } from "react";
-import HeaderButton from "../HeaderButton/HeaderButton";
-import "./Account.scss";
+import { useDispatch, useSelector } from "react-redux";
 
-const Header = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+import { updateToken } from "../../../../../../redux/features/authentication/authentication";
+import { useAuthCheckQuery, useUserDataQuery } from "../../../../../../redux/services/core/core";
+import { RootState } from "../../../../../../redux/store";
+import { decodeString } from "../../../../../../utils";
+import Spinner from "../../../../../Spinner/Spinner";
+import HeaderButton from "../HeaderButton/HeaderButton";
+import "./UserButton.scss";
+
+const UserButton = () => {
+  const dispatch = useDispatch();
+
+  // Store data
+  // ========================================
+  const authToken = decodeString(useSelector((state: RootState) => state.authentication.token)) ?? '';
+  // ==========
+
+  // Api Data
+  // ========================================
+  const { data: authCheckData, isLoading: authCheckLoading } = useAuthCheckQuery(authToken, {skip: (authToken === '')});
+  const { data: userData, isLoading: userDataIsLoading } = useUserDataQuery(authToken, {skip: (authToken === '')});
+  const authenticated = authCheckData?.authenticated ?? false;
+  const {image, name, userType} = userData ?? {image: '', name: '-', userType: 'standard'};
+  const isPremium = userType === "premium";
+  const isLoading = authCheckLoading || userDataIsLoading;
+  // ==========
 
   return (
-    <div id="account">
-      {!loggedIn && <HeaderButton onClick={() => {}}>Log In</HeaderButton>}
-      {loggedIn && <HeaderButton onClick={() => {}}></HeaderButton>}
+    <div id="user-button" className={isPremium ? 'premium' : ''}>
+      <HeaderButton onClick={() => {
+        // TODO: Move this to login form submit
+        dispatch(updateToken('bulbasaur'));
+      }}>
+        {!authenticated && <span id="user-login">Log In</span>}
+
+        {authenticated && (
+          <div id="user-container">
+            <div id="user-image">
+              {isLoading && <Spinner />}
+              {!isLoading && <img src={image} alt="user image" />}
+            </div>
+
+            <div id="user-text">
+              <div id="user-name">{name}</div>
+
+              <div id="user-type">
+                {isPremium ? "Premium Member" : "Standard Member"}
+              </div>
+            </div>
+          </div>
+        )}
+      </HeaderButton>
     </div>
   );
 };
 
-export default Header;
+export default UserButton;

2c3f761d3c3597319d7f77ef49ed728089951bf8
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-07 16:59:44 +0000

    Improve keyboard navigation in header

--- src/components/Frame/components/Header/components/HeaderButton/HeaderButton.tsx
+++ src/components/Frame/components/Header/components/HeaderButton/HeaderButton.tsx
@@ -1,14 +1,23 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 import "./HeaderButton.scss";
 
 type Props = {
   onClick: () => void;
+  onFocus: React.FocusEventHandler<HTMLButtonElement> | undefined;
+  onBlur: React.FocusEventHandler<HTMLButtonElement> | undefined;
 };
 
-const HeaderButton = ({ children, onClick }: PropsWithChildren<Props>) => {
+const HeaderButton = ({
+  children,
+  onClick,
+  onFocus,
+  onBlur,
+}: PropsWithChildren<Props>) => {
   return (
     <div id="header-button">
-      <button onClick={onClick}>{children}</button>
+      <button onFocus={onFocus} onBlur={onBlur} onClick={onClick}>
+        {children}
+      </button>
     </div>
   );
 };

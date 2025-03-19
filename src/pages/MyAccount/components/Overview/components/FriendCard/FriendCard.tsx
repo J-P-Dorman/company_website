17b242324c159fd00dabc59b1b4a28b627017610
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-19 20:10:13 +0000

    Squash me into previous

---
+++ src/pages/MyAccount/components/Overview/components/FriendCard/FriendCard.tsx
@@ -0,0 +1,30 @@
+import Button from "../../../../../../components/Button/Button";
+import "./FriendCard.scss";
+
+type Props = {
+  image: string;
+  name: string;
+  description: string;
+	onClick: () => void;
+};
+
+const Overview = ({ image, name, description, onClick }: Props) => {
+  return (
+    <div className="friend-card">
+			<Button type="button" className="btn-clear"  onClick={onClick}>
+			<div className="card">
+        <div className="background-image" style={{ backgroundImage: `url("${image}")` }}>
+     
+          </div> 
+        <div className="friend-text">
+          <div className="friend-name">{name}</div>
+          <div className="friend-description">{description}</div>
+        </div>
+      </div>
+			</Button>
+  
+    </div>
+  );
+};
+
+export default Overview;
---
+++ src/pages/MyAccount/components/Overview/components/FriendCardMore/FriendCardMore.tsx
@@ -0,0 +1,25 @@
+import image from "../../../../../../assets/background_primary.jpg";
+import Button from "../../../../../../components/Button/Button";
+import "./FriendCardMore.scss";
+import chevronRight from '../../../../../../assets/icons/chevron-right.svg';
+
+type Props = {
+  onClick: () => void;
+};
+
+const Overview = ({ onClick }: Props) => {
+  return (
+    <div className="friend-card-more">
+      <Button type="button" className="btn-clear" onClick={onClick}>
+        <div className="card" style={{ backgroundImage: `url("${image}")` }}>
+          <div className="text">
+            <span>See All</span>
+            <img src={chevronRight} alt="chevron right" />
+          </div>
+        </div>
+      </Button>
+    </div>
+  );
+};
+
+export default Overview;

2350f3fa8f149176d76d90a7ce0913f8eb349c6d
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-19 20:11:23 +0000

    Move file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import Description from "./components/Description/Description";
 import Footer from "../../components/Footer/Footer";
 import Newsletter from "./components/Newsletter/Newsletter";
 import "./Home.scss";
-import Content from "./components/Content/Content";
+import Content from "../../components/Content/Content";
 import Login from "../../components/Login/Login";
 
 const Home = () => {

6757aca24f034d1ebadca52ba4f5c1fe103c7cff
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-19 20:05:09 +0000

    format file

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Input.scss";
 
 type Props = {
@@ -8,15 +8,22 @@ type Props = {
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input = ({ label, type = 'text', value: valueParent, onChange }: Props) => {
+const Input = ({
+  label,
+  type = "text",
+  value: valueParent,
+  onChange,
+}: Props) => {
   const [valueLocal, setValueLocal] = useState("");
   const [isFocus, setIsFocus] = useState(false);
 
   const value = valueParent === undefined ? valueLocal : valueParent;
-  const isEmpty = value === '';
+  const isEmpty = value === "";
 
   return (
-    <div className={`input${isFocus ? " focus" : ""}${isEmpty ? " empty" : ""}`}>
+    <div
+      className={`input${isFocus ? " focus" : ""}${isEmpty ? " empty" : ""}`}
+    >
       <div className="input-border">
         <div className="input-label">
           <div className="input-label-text">{label}</div>
@@ -28,7 +35,7 @@ const Input = ({ label, type = 'text', value: valueParent, onChange }: Props) =>
         value={value}
         onChange={(event) => {
           setValueLocal(event.target.value);
-          if(onChange) onChange(event);
+          if (onChange) onChange(event);
         }}
         onFocus={() => {
           setIsFocus(true);

815362daff5ec9554e9c6f763ea147552ceffdfb
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-03-24 18:31:34 +0000

    Kick user to home when logged out

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import "./App.scss";
 import Home from "./pages/Home/Home";
-import { store } from "./redux/store";
 import MyAccount from "./pages/MyAccount/MyAccount";
+import { useAuthCheckQuery } from "./redux/services/core/core";
+import { RootState } from "./redux/store";
+import { decodeString } from "./utils";
 
 const App = () => {
+  // Store data
+  // ========================================
+  const authToken =
+    decodeString(
+      useSelector((state: RootState) => state.authentication.token)
+    ) ?? "";
+  // ==========
+
+  // Api Queries
+  // ========================================
+  const { data: authCheckData } = useAuthCheckQuery(authToken);
+  const isAuthenticated = authCheckData?.authenticated ?? false;
+  // ==========
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/my-account" element={<MyAccount />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {isAuthenticated && (
+          <>
+            <Route path="/my-account" element={<MyAccount />} />
+          </>
+        )}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

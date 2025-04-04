df71449469d9073e3f3234c3167504761bf0ae9e
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-04-04 12:10:29 +0100

    Replace hero image

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -1,10 +1,15 @@
-import HeroImage from "../../../../assets/hero.png";
+import HeroImage from "../../../../assets/hero.jpg";
 import "./Hero.scss";
 
 const Home = () => {
   return (
-    <div id="hero">
-      <img src={HeroImage} alt="Hero image" />
+    <div id="hero" style={{backgroundImage: `url('${HeroImage}')`}}>
+      <div id="hero-text">
+      <div id="hero-header">Choose Your Adventure</div>
+      <div id="hero-subheader">Alpha test now live!</div>
+      <div id="hero-body">Log in / Play now today!</div>
+      <div id="hero-disclaimer">(Don't worry, no fields are connected to the api)</div>
+      </div>
     </div>
   );
 };

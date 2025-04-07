d432b2fad6d8b558c8fee6dc388d85793eea4615
Joe Dorman <67368331+J-P-Dorman@users.noreply.github.com>
2025-04-07 19:05:44 +0100

    Add mobile menu to header

--- src/components/Frame/components/Header/Header.tsx
+++ src/components/Frame/components/Header/Header.tsx
@@ -4,23 +4,83 @@ import PlayNow from "./components/PlayNow/PlayNow";
 import "./Header.scss";
 import HeaderButton from "./components/HeaderButton/HeaderButton";
 import { Link } from "react-router";
+import Sheet from "./components/Sheet/Sheet";
+import Button from "../../../Button/Button";
+import { useState } from "react";
+import { useBreakpoint, useResize } from "../../../../utils";
+import imgMenu from "../../../../assets/icons/bars.svg";
 
 const Header = () => {
+  const breakpoint = useBreakpoint();
+  const isMobile = breakpoint === 'xs' || breakpoint === 's';
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div id="header">
       <div id="header-container">
         <div id="header-left">
           <div id="header-logo">
             <Link to="/">
-              <img src={companyLogo} className="logo" alt="Games Company Logo" />
+              <img
+                src={companyLogo}
+                className="logo"
+                alt="Games Company Logo"
+              />
             </Link>
-        </div>
+          </div>
         </div>
 
         <div id="header-right">
-          <UserButton />
-          <HeaderButton onClick={() => {}}>News</HeaderButton>
-          <PlayNow />
+          {!isMobile && (
+            <>
+              <UserButton />
+              <HeaderButton onClick={() => {}}>News</HeaderButton>
+              <PlayNow />
+            </>
+          )}
+
+          {isMobile && (
+            <>
+              <Button
+                className="btn-clear"
+                onClick={() => {
+                  setIsOpen((prev) => !prev);
+                }}
+              >
+                <img className="icon" src={imgMenu} alt="menu" />
+              </Button>
+              <Sheet
+                align="right"
+                type="primary"
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+              >
+                <>
+                  <Button
+                    className="btn-clear"
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    Log In
+                  </Button>
+                  <Button
+                    className="btn-clear"
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    News
+                  </Button>
+                  <Button
+                    className="btn-clear"
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    Play Now
+                  </Button>
+                </>
+              </Sheet>
+            </>
+          )}
         </div>
       </div>
     </div>
